262290c284b9151d10daefe208b7e2c0a4d33c7f
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2022-10-22 21:29:45 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener(
+    function () {
+        /* play video twice as fast */
+        document.querySelector('video').defaultPlaybackRate = 2.0;
+        document.querySelector('video').play();
+        /* now play three times as fast just for the heck of it */
+        document.querySelector('video').playbackRate = 16.0;
+    }
+);
